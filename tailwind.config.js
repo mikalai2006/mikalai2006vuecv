3a44504f87dbdc82cd1d5cdcfb9e9ac4e7805680
Mikalai Parakhnevich <composer.opencart@gmail.com>
2022-03-10 17:51:31 +0300

    feat: start project

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+const colors = require("tailwindcss/colors");
+module.exports = {
+  content: [],
+  theme: {
+    colors: {
+      default: colors.neutral,
+      primary: colors.green,
+    },
+    extend: {},
+  },
+  plugins: [],
+};
